13b2dfa74c62170205801f0e44cbe53ee80e57d1
nisone <nuhu94@gmail.com>
2022-05-28 23:11:38 +0100

    Added welcome component to app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    Welcome()
   );
+}
+
+export function Welcome() {
+  const element = <p>Welcome to reactjs</p>
+
+  return (
+  <div className="App">
+  <header className="App-header">
+    <div>
+      {element}
+    </div>
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+</div>
+)
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Welcome from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('render welcom to reactjs', () => {
+  render(<Welcome />);
+  const welcomeText = screen.getByText(/Welcome to reactjs/i);
+  expect(welcomeText).toBeInTheDocument();
+});
+
